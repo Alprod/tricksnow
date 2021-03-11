45a59160c56428dec33b2684be18ff90f5b24172
Alain <alprod81@gmail.com>
2021-03-11 23:17:05 +0100

    Created entity video

---
+++ migrations/Version20210311221220.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210311221220 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, figures_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, link VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_C53D045F5C7F3A37 (figures_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE video (id INT AUTO_INCREMENT NOT NULL, figures_id INT DEFAULT NULL, title VARCHAR(90) DEFAULT NULL, link VARCHAR(255) DEFAULT NULL, INDEX IDX_7CC7DA2C5C7F3A37 (figures_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE image ADD CONSTRAINT FK_C53D045F5C7F3A37 FOREIGN KEY (figures_id) REFERENCES figure (id)');
+        $this->addSql('ALTER TABLE video ADD CONSTRAINT FK_7CC7DA2C5C7F3A37 FOREIGN KEY (figures_id) REFERENCES figure (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE image');
+        $this->addSql('DROP TABLE video');
+    }
+}
---
+++ src/Entity/Video.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VideoRepository;
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+use Monolog\DateTimeImmutable;
+
+/**
+ * @ORM\Entity(repositoryClass=VideoRepository::class)
+ */
+class Video
+{
+    use ResourceId;
+
+    /**
+     * @ORM\Column(type="string", length=90, nullable=true)
+     */
+    private ?string $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $link;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Figure::class, inversedBy="videos")
+     */
+    private ?Figure $figures;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTimeInterface $created_at;
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTimeImmutable();
+    }
+
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(?string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getFigures(): ?Figure
+    {
+        return $this->figures;
+    }
+
+    public function setFigures(?Figure $figures): self
+    {
+        $this->figures = $figures;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+}
