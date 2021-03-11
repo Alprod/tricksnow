494a11b8bf8764249bb6cf96f5534ff170d5dcb9
Alprod <alprod81@gmail.com>
2021-03-11 18:26:19 +0100

    Ajout recource de date de création et mise a jour de la date

--- src/Entity/Timestapable.php
+++ src/Entity/Timestapable.php
@@ -4,7 +4,41 @@
 namespace App\Entity;
 
 
-class Timestapable
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+trait Timestapable
 {
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTimeInterface $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTimeInterface $updateAt;
+
+
+    public function getCreatedAt(): DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+
+    public function getUpdateAt(): DateTimeInterface
+    {
+        return $this->updateAt;
+    }
+
+    /**
+     * @param DateTimeInterface $updateAt
+     */
+    public function setUpdateAt(DateTimeInterface $updateAt): void
+    {
+        $this->updateAt = $updateAt;
+    }
+
+
 
 }
